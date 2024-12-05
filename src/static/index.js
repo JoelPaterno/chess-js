2d05ff1b07c32aaf4e5d4cf2b6bdf173b98481b3
root <root@LAPTOP-QAOG4NLH.>
2024-12-05 16:21:09 +1100

    load board from js file

--- src/static/index.js
+++ src/static/index.js
@@ -1,6 +1,7 @@
 const resetButton = document.getElementsByClassName("reset-button")[0];
 var boxes = document.getElementsByClassName("box");
-//onload = (event) => {setPlayerBlack()};
+const rank_pieces = ["rook", "knight", "bishop", "queen", "king", "bishop", "knight", "rook"]
+onload = (event) => {setPlayerWhite()};
 
 function clearBoard() {
     for (const i in boxes) {
@@ -11,13 +12,13 @@ function clearBoard() {
 function setPlayerWhite() {
     for (const i in boxes) {
         if (i < 8) {
-            boxes[i].innerHTML = "" + i + " b rank";
+            boxes[i].innerHTML = `<img class="piece" id="${rank_pieces[i]}${i}-b" src="static/pieces/${rank_pieces[i]}-b.svg" ondragstart="drag(event);" draggable="true"/>`;
         } else if (i > 7 && i < 16 ) {
             boxes[i].innerHTML = `<img class="piece" id="pawn${i}-b" src="static/pieces/pawn-b.svg" ondragstart="drag(event);" draggable="true"/>`;
         } else if (i > 47 && i < 56) {
-            boxes[i].innerHTML = `<img class="piece" id="pawn${i}-b" src="static/pieces/pawn-w.svg" ondragstart="drag(event);" draggable="true"/>`;
+            boxes[i].innerHTML = `<img class="piece" id="pawn${i}-w" src="static/pieces/pawn-w.svg" ondragstart="drag(event);" draggable="true"/>`;
         } else if (i > 55 && i < 64) {
-            boxes[i].innerHTML = "" + i + " w rank";
+            boxes[i].innerHTML = `<img class="piece" id="${rank_pieces[Math.abs(56-i)]}${i}-w" src="static/pieces/${rank_pieces[Math.abs(56-i)]}-w.svg" ondragstart="drag(event);" draggable="true"/>`;
         }
     }
 }
@@ -26,13 +27,13 @@ function setPlayerBlack() {
     for (const i in boxes) {
         for (const i in boxes) {
             if (i < 8) {
-                boxes[i].innerHTML = "" + i + " b rank";
+                boxes[i].innerHTML = `<img class="piece" id="${rank_pieces[i]}${i}-w" src="static/pieces/${rank_pieces[i]}-w.svg" ondragstart="drag(event);" draggable="true"/>`
             } else if (i > 7 && i < 16 ) {
-                boxes[i].innerHTML = `<img class="piece" id="pawn${i}-b" src="static/pieces/pawn-w.svg" ondragstart="drag(event);" draggable="true"/>`;
+                boxes[i].innerHTML = `<img class="piece" id="pawn${i}-w" src="static/pieces/pawn-w.svg" ondragstart="drag(event);" draggable="true"/>`;
             } else if (i > 47 && i < 56) {
                 boxes[i].innerHTML = `<img class="piece" id="pawn${i}-b" src="static/pieces/pawn-b.svg" ondragstart="drag(event);" draggable="true"/>`;
             } else if (i > 55 && i < 64) {
-                boxes[i].innerHTML = "" + i + " w rank";
+                boxes[i].innerHTML = `<img class="piece" id="${rank_pieces[i+55]}${i}-b" src="static/pieces/${rank_pieces[i+55]}-b.svg" ondragstart="drag(event);" draggable="true"/>`;
             }
         }
     }
