f53ce515286cf7690688099432133b26962d498d
root <root@LAPTOP-QAOG4NLH.>
2024-12-05 15:43:48 +1100

    clear board function

--- src/static/index.js
+++ src/static/index.js
@@ -1,3 +1,26 @@
+const resetButton = document.getElementsByClassName("reset-button")[0];
+var boxes = document.getElementsByClassName("box")
+
+function clearBoard() {
+    for (const i in boxes) {
+        boxes[i].innerHTML = "";
+        console.log(boxes[i].innerHTML);
+    }
+}
+
+function setPlayerWhite() {
+
+}
+
+function setPlayerBlack() {
+    
+}
+
+resetButton.addEventListener("click", (event) => {
+    console.log("reset board clicked");
+    clearBoard();
+})
+
 function allowDrop(ev) {
     ev.preventDefault();
 }
@@ -12,3 +35,5 @@ function drop(ev) {
     ev.target.appendChild(document.getElementById(data));
     console.log(data + " moved to " + ev.target.id);
 }
+
+
