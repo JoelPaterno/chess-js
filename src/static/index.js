ba76084093de3913b61d7d41e05ff5a86e904dea
root <root@LAPTOP-QAOG4NLH.>
2024-12-05 16:01:22 +1100

    added JS board setting functions

--- src/static/index.js
+++ src/static/index.js
@@ -1,5 +1,6 @@
 const resetButton = document.getElementsByClassName("reset-button")[0];
-var boxes = document.getElementsByClassName("box")
+var boxes = document.getElementsByClassName("box");
+//onload = (event) => {setPlayerBlack()};
 
 function clearBoard() {
     for (const i in boxes) {
@@ -9,19 +10,38 @@ function clearBoard() {
 
 function setPlayerWhite() {
     for (const i in boxes) {
-
+        if (i < 8) {
+            boxes[i].innerHTML = "" + i + " b rank";
+        } else if (i > 7 && i < 16 ) {
+            boxes[i].innerHTML = `<img class="piece" id="pawn${i}-b" src="static/pieces/pawn-b.svg" ondragstart="drag(event);" draggable="true"/>`;
+        } else if (i > 47 && i < 56) {
+            boxes[i].innerHTML = `<img class="piece" id="pawn${i}-b" src="static/pieces/pawn-w.svg" ondragstart="drag(event);" draggable="true"/>`;
+        } else if (i > 55 && i < 64) {
+            boxes[i].innerHTML = "" + i + " w rank";
+        }
     }
 }
 
 function setPlayerBlack() {
     for (const i in boxes) {
-
+        for (const i in boxes) {
+            if (i < 8) {
+                boxes[i].innerHTML = "" + i + " b rank";
+            } else if (i > 7 && i < 16 ) {
+                boxes[i].innerHTML = `<img class="piece" id="pawn${i}-b" src="static/pieces/pawn-w.svg" ondragstart="drag(event);" draggable="true"/>`;
+            } else if (i > 47 && i < 56) {
+                boxes[i].innerHTML = `<img class="piece" id="pawn${i}-b" src="static/pieces/pawn-b.svg" ondragstart="drag(event);" draggable="true"/>`;
+            } else if (i > 55 && i < 64) {
+                boxes[i].innerHTML = "" + i + " w rank";
+            }
+        }
     }
 }
 
 resetButton.addEventListener("click", (event) => {
     console.log("reset board clicked");
     clearBoard();
+    setPlayerWhite();
 })
 
 function allowDrop(ev) {
